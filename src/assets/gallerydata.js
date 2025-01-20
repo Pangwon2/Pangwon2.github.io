7b2950055d890ccdf5b1f90ede05ed5bbd39dcbd
leesangwon <leesangwon1@itofrom.com>
2025-01-20 15:25:13 +0900

    필터링구현

--- src/assets/gallerydata.js
+++ src/assets/gallerydata.js
@@ -1,28 +1,164 @@
 export default [
-    {
-      price: 30000,
-      postImage: require('../assets/myodam.jpg'),
-      likes : 30,
-      liked: false,
-      content: `가성비`,
-      filter: "wedding",
-      color: "white"
-    },
-    {
-      price: 50000,
-      postImage: require('../assets/gift_black.jpg'),
-      likes: 20,
-      liked: false,
-      content: `보급형`,
-      filter: "gift",
-      color: "black"
-    },
-    {price: 100000,
-      postImage: require('../assets/glass.jpg'),
-      likes: 49,
-      liked: false,
-      content: `고급형`,
-      filter: "gift",
-      color: "blue"
-    }
-  ]
+  {
+    price: 30000,
+    postImage: require('../assets/myodam.jpg'),
+    likes: 30,
+    liked: false,
+    content: `가성비`,
+    filter: "wedding",
+    color: "white"
+  },
+  {
+    price: 50000,
+    postImage: require('../assets/gift_black.jpg'),
+    likes: 20,
+    liked: false,
+    content: `보급형`,
+    filter: "gift",
+    color: "black"
+  },
+  {
+    price: 100000,
+    postImage: require('../assets/glass.jpg'),
+    likes: 49,
+    liked: false,
+    content: `고급형`,
+    filter: "gift",
+    color: "blue"
+  },
+  {
+    price: 45000,
+    postImage: require('../assets/myodam2.jpg'),
+    likes: 25,
+    liked: false,
+    content: `화려한`,
+    filter: "wedding",
+    color: "red"
+  },
+  {
+    price: 55000,
+    postImage: require('../assets/myodam3.jpg'),
+    likes: 35,
+    liked: false,
+    content: `친환경`,
+    filter: "gift",
+    color: "green"
+  },
+  {
+    price: 60000,
+    postImage: require('../assets/sample_images_00.png'),
+    likes: 40,
+    liked: false,
+    content: `로맨틱`,
+    filter: "wedding",
+    color: "black"
+  },
+  {
+    price: 70000,
+    postImage: require('../assets/sample_images_01.png'),
+    likes: 45,
+    liked: false,
+    content: `밝은`,
+    filter: "gift",
+    color: "yellow"
+  },
+  {
+    price: 80000,
+    postImage: require('../assets/sample_images_02.png'),
+    likes: 50,
+    liked: false,
+    content: `우아한`,
+    filter: "wedding",
+    color: "white"
+  },
+  {
+    price: 90000,
+    postImage: require('../assets/sample_images_03.png'),
+    likes: 55,
+    liked: false,
+    content: `활기찬`,
+    filter: "gift",
+    color: "red"
+  },
+  {
+    price: 75000,
+    postImage: require('../assets/sample_images_04.png'),
+    likes: 60,
+    liked: false,
+    content: `세련된`,
+    filter: "wedding",
+    color: "blue"
+  },
+  {
+    price: 85000,
+    postImage: require('../assets/sample_images_05.png'),
+    likes: 65,
+    liked: false,
+    content: `따뜻한`,
+    filter: "gift",
+    color: "green"
+  },
+  {
+    price: 95000,
+    postImage: require('../assets/sample_images_06.png'),
+    likes: 70,
+    liked: false,
+    content: `신선한`,
+    filter: "wedding",
+    color: "black"
+  },
+  {
+    price: 105000,
+    postImage: require('../assets/sample_images_07.png'),
+    likes: 75,
+    liked: false,
+    content: `화사한`,
+    filter: "gift",
+    color: "red"
+  },
+  {
+    price: 115000,
+    postImage: require('../assets/sample_images_08.png'),
+    likes: 80,
+    liked: false,
+    content: `산뜻한`,
+    filter: "wedding",
+    color: "white"
+  },
+  {
+    price: 125000,
+    postImage: require('../assets/sample_images_09.png'),
+    likes: 85,
+    liked: false,
+    content: `차분한`,
+    filter: "gift",
+    color: "yellow"
+  },
+  {
+    price: 135000,
+    postImage: require('../assets/sample_images_10.png'),
+    likes: 90,
+    liked: false,
+    content: `고전적인`,
+    filter: "wedding",
+    color: "white"
+  },
+  {
+    price: 145000,
+    postImage: require('../assets/sample_images_11.png'),
+    likes: 95,
+    liked: false,
+    content: `깊은`,
+    filter: "gift",
+    color: "white"
+  },
+  {
+    price: 155000,
+    postImage: require('../assets/sample_images_12.png'),
+    likes: 100,
+    liked: false,
+    content: `자연스러운`,
+    filter: "wedding",
+    color: "blue"
+  }
+];
